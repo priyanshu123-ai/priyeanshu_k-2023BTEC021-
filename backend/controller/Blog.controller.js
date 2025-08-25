d2cb90c82c1ed239a90cd1f7788ecd46ddf0f669
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-08-25 16:33:29 +0530

    blogcontroller

--- backend/controller/Blog.controller.js
+++ backend/controller/Blog.controller.js
@@ -1,16 +1,70 @@
-export const createBlog = async (req,res) => {
-    try {
-        const {title,content} = req.body;
+import { Blog } from "../model/blog.js";
+import cloudniary from "../utils/Cloudniary.js";
+import getDataUri from "../utils/datauri.js";
 
-        if(!title || !content){
-            return res.status(401).json({
-                success:false,
-                message:"All content required"
-            })
-        }
+export const createBlog = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(401).json({
+        success: false,
+        message: "All content required",
+      });
+    }
+    let imageUrl = "";  // when we upload key of file name must be file
+    if (req.file) {
+      const fileUrl = getDataUri(req.file);
+      const cloudResponse = await cloudniary.uploader.upload(fileUrl.content, {
+        resource_type: "auto",
+      });
+      imageUrl = cloudResponse.secure_url;
+    }
+
+    const blog = await Blog.create({
+      title,
+      content,
+      imageUrl,
+      user: req.userId,
+    });
+    const populatedBlog = await Blog.findById(blog._id).populate(
+      "user",
+      "name email"
+    );
+    return res.status(201).json({
+      success: true,
+      message: "Blog created successfully",
+      blog: populatedBlog,
+    });
+  } catch (error) {
+    console.log(error);
+ 
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllBlogs = async (req,res) => {
+    try {
+        const blogs = await Blog.find()
+          return res.status(201).json({
+      success: true,
+      message: "Blog created successfully",
+      blogs
+    });
+
+
         
     } catch (error) {
-        console.log(error)
+        console.log(error);
+ 
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  
         
     }
 }
