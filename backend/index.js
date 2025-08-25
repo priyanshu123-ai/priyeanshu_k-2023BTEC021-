b3905f99814684e17b0eb68e914f33c0ae3d3404
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-08-25 14:38:23 +0530

    update_index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import database from "./utils/database.js"
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT || 4000
+
+database()
+
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
 })
