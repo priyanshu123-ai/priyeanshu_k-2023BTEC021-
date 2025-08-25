90e21efd56759b2b7dc1baa2d25db93ecf59c8f3
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-08-25 15:12:04 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
 import database from "./utils/database.js"
+import userRoute from "./route/user.route.js"
+
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT || 4000
+app.use(express.json())
+
+
 
 database()
+
+app.use("/api/v1",userRoute)
 
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
