fe0e3526bdfa84f43fdb00cf9194d51de44f7682
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-08-25 15:04:45 +0530

    userController

--- backend/controller/User.controller.js
+++ backend/controller/User.controller.js
@@ -1,3 +1,6 @@
+import { User } from "../model/User.js";
+import bcrypt  from "bcryptjs"
+import sendRegisterEmail from "../utils/nodemailer.js";
 
 
 export const register = async(req,res) => {
@@ -11,8 +14,39 @@ export const register = async(req,res) => {
                 message:"All field are required"
             })
         }
+
+        const existUser = await User.findOne({email})
+
+        if(!existUser){
+            return res.status(401).json({
+                success:false,
+                message:"User Already exist"
+            })
+        }
+
+        const hashPassword = await bcrypt.hash(password,10)
+
+        await sendRegisterEmail(email)
+
+        const user = await User.create({
+            name:name,
+            email:email,
+            password:hashPassword,
+
+        })
+
+        return res.status(201).json({
+            success:true,
+            message:"User created successfully",
+            user
+        })
         
     } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
         
     }
 }
