f54714f35dc552d0a3a58eb71739407690b9b4a4
Priyanshu Kumar <priyanshukumarpriyanshu015@gmail.com>
2025-08-25 16:29:04 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import database from "./utils/database.js"
 import userRoute from "./route/user.route.js"
+import blogRoute from "./route/blog.route.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -10,11 +12,15 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(express.json())
 
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+
 
 
 database()
 
 app.use("/api/v1",userRoute)
+app.use("/api/v2",blogRoute)
 
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
